5bc27c7fe2ac549e8d2a7007539a18ec79915de8
Halyna Melnyk <39273210+mehalyna@users.noreply.github.com>
2024-01-12 20:05:00 +0200

    Update TemperatureConverter.cs

--- Practics1/TemperatureConverter.cs
+++ Practics1/TemperatureConverter.cs
@@ -3,15 +3,7 @@ namespace Practics1
 {
     public class TemperatureConverter
     {
-        public double FahrenheitToCelsius(double fahrenheit)
-        {
-            return (fahrenheit - 32) * 5 / 9;
-        }
-
-        public double CelsiusToFahrenheit(double celsius)
-        {
-            return (celsius * 9 / 5) + 32;
-        }
+         // Your code here
     }
 
 }
