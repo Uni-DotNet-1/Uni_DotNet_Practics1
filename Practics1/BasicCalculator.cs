a179bd1aeed13f9a7dd6f92ed9f8a63c405c5cd6
Halyna Melnyk <39273210+mehalyna@users.noreply.github.com>
2024-01-12 20:04:14 +0200

    Update BasicCalculator.cs

--- Practics1/BasicCalculator.cs
+++ Practics1/BasicCalculator.cs
@@ -5,19 +5,7 @@ namespace Practics1
     {
         public double Calculate(double num1, double num2, string operation)
         {
-            switch (operation)
-            {
-                case "+":
-                    return num1 + num2;
-                case "-":
-                    return num1 - num2;
-                case "*":
-                    return num1 * num2;
-                case "/":
-                    return num2 != 0 ? num1 / num2 : double.NaN;
-                default:
-                    throw new InvalidOperationException("Invalid operation");
-            }
+            // Your code here
         }
     }
 
